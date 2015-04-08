80b54c400b09c2c9c83faf6bd4ac2376dee3ae95
townmi <towne766@126.com>
2015-04-08 17:22:33 +0800

    add 17:22

--- z_spider/route/index.js
+++ z_spider/route/index.js
@@ -1,3 +1,5 @@
+var cp = require("child_process");
+
 var express = require('express');
 var superagent = require("superagent");
 // var needle = require("needle");
@@ -16,74 +18,94 @@ var router = express.Router();
 // exports
 module.exports = router;
 
-// router.get("/", function (req, res){
+router.get("/", function (req, res){
 
-	superagent.get('http://www.jd.com/').end(function (error, body){
+	var d1 = new Date().getTime();
+
+	superagent.get('http://www.goubaa.com/login/').set({'API-Key': 'foobar', Accept: 'application/json'}).end(function (error, body){
+
+		// console.log(body.request.url);
 		var $ = cheerio.load(body.text);
-		// var len = $("a").length, index = 0, timer = null;
-		// console.log(len)
-		// function getE (i, e){
-			
-		// 	return superagent.get(e).end(function (error, b){
-		// 		if(error) return index++;
-		// 		console.log(i,b.text.substring(0,19));
-		// 		index++;
-		// 		return b.text;
-		// 	});
-		// }
-		// var result = [];
 
-		// $("a").each(function (i, e){
-
-		// 	result.push(getE(i, e.href));
-
-		// })
-
-		// timer = setInterval(function(){
-		// 	console.log(result);
-		// 	if(index >= len-1){
-		// 		clearInterval(timer);
-		// 		res.render("index",{key: result});
-		// 	}
-		// },1000)
 		var result = [];
 
 		$("a").each(function (i,e){
 			if(e.attribs.href){
-
-				// console.log(e.attribs.href.match(/javascript/g));
-				if(e.attribs.href.match(/javascript/g)){
-
-				}else{
+				if(!e.attribs.href.match(/javascript/g)){
 					result.push(e.attribs.href);
 				}
-
 			}
-			
-			// if(e.attribs.href.match(/javascript/g)){
-
-			// }else{
-			// 	result.push(e.attribs.href);
-			// }
-			
 		})
-
-		console.log(result.length);
-
 
 		// ep 并发
 		ep.after('superagent', result.length, function (list) {
-		// 在所有文件的异步执行结束后将被执行
-		// 所有文件的内容都存在list数组中
-			console.log(list[1].res.text);
+			// console.log(list);
+
+
+			var last = [];
+			list.forEach(function (ele){
+				if(ele){
+					var body = ele.blob.toString();
+					//console.log("这里是解析DOM节点")
+					//mem();
+					var $ = cheerio.load(body);
+					var html = $("body").html();
+					// console.log(html)
+					if(typeof html === "string" && html.match(/1/gi)){
+						last.push(ele.url);
+					}
+
+					body = $ = html = null;
+				}
+				
+			});
+			// cp.spawn(res(list));
+			console.log("爬取首页有效连接共："+result.length+"条", "解析后页面共有链接："+last.length+"条");
+			console.log(list[2].url);
+			var d2 = new Date().getTime();
+			res.render("index",{key: last});
+			mem();
+			return console.log("耗时"+(d2-d1)/1000+"秒");
+
 		});
 		for (var i = 0; i < result.length; i++) {
-			superagent.get(result[i]).end(function (err, body){
-		// 触发结果事件
-				ep.emit('superagent', body);
+
+			superagent.get(result[i]).set({'API-Key': 'foobar', Accept: 'application/json'}).end(function (err, body){
+				// 触发结果事件
+				console.log("这里在发起GET请求");
+				mem();
+				if(body && body.res && body.res.text){
+					
+					ep.emit('superagent', {blob: new Buffer(body.res.text), url: body.request.url});
+				}else{
+					// console.log(err, body);
+					ep.emit("superagent", "")
+				}
+				
 			});
 		}
 
+		// function res(arr){
+		// 	var result = [];
+		// 	arr.forEach(function (e){
+		// 		console.log("---->for________>>>>>>>")
+		// 		mem();
+		// 		var $ = cheerio.load(e);
+		// 		$("a").each(function (i,e){
+		// 			if(e.attribs.href){
+		// 				if(e.attribs.href.match(/javascript/g)){
+
+		// 				}else{
+		// 					result.push(e.attribs.href);
+		// 				}
+
+		// 			}
+					
+		// 		})
+		// 	});
+
+		// 	return result;
+		// }
 
 		// Promise.promisifyAll(superagent);
 		// var options = {};
@@ -117,7 +139,7 @@ module.exports = router;
 
 	mem();
 
-// });
+});
 // function foo(result) {
 //     console.log(result);
 //     return result+result;
